d268c3ff542bb353e562164907b459a76d2a9fa2
Yubin <ryzond6728@gmail.com>
2021-07-11 15:11:11 +0900

    chore: yarn berry 적용 및 next.js 버전 11로 업데이트

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,12 +39,6 @@ export default class MyDocument extends Document {
       <Html lang="ko">
         <Head />
         <body>
-          <script
-            dangerouslySetInnerHTML={{
-              __html:
-                '!function(){const e="dark-mode",t="light-mode";function o(o){document.documentElement.classList.add(o?e:t),document.documentElement.classList.remove(o?t:e)}const c=window.matchMedia("(prefers-color-scheme: dark)"),a="(prefers-color-scheme: dark)"===c.media;let s=null;try{s=localStorage.getItem("darkMode")}catch(e){}const l=null!==s;if(l&&(s=JSON.parse(s)),l)o(s);else if(a)o(c.matches),localStorage.setItem("darkMode",c.matches),localStorage.setItem("preferDarkMode",c.matches);else{const t=document.documentElement.classList.contains(e);localStorage.setItem("darkMode",JSON.stringify(t))}}();',
-            }}
-          />
           <Main />
           <NextScript />
         </body>

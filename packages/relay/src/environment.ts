ac5eaf0409fbec1c31a7c73ff7225f46339fdb71
Yubin <ryzond6728@gmail.com>
2021-08-17 00:37:11 +0900

    chore(relay): relayEnvironmentProvider 설정

---
+++ packages/relay/src/environment.ts
@@ -0,0 +1,23 @@
+import { Environment, Network, RecordSource, Store } from "relay-runtime";
+import type { RequestParameters } from "relay-runtime/lib/util/RelayConcreteNode";
+import type { Variables } from "relay-runtime/lib/util/RelayRuntimeTypes";
+
+async function fetchGraphQL(params: RequestParameters, variables: Variables) {
+  const response = await fetch("https://api.graphql.jobs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: params.text,
+      variables,
+    }),
+  });
+
+  return await response.json();
+}
+
+export default new Environment({
+  network: Network.create(fetchGraphQL),
+  store: new Store(new RecordSource()),
+});
--- packages/relay/src/index.tsx
+++ packages/relay/src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { RelayEnvironmentProvider } from "react-relay";
 import App from "./App";
+import Environment from "./environment";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RelayEnvironmentProvider environment={Environment}>
+      <App />
+    </RelayEnvironmentProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
